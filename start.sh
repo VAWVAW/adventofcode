eaec45c1592e2d3bdbf25d478774eee90c681a67
vawvaw <valentin@wiedekind1.de>
2023-12-04 11:22:07 +0100

    solve 04

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ if [ ! -d "$DATE" ]; then
   cp template.rs $DATE/src/main.rs
   touch $DATE/data.txt
   touch $DATE/data_test1.txt
-  touch $DATE/data_test2.txt
+  ln -s $DATE/data_test1.txt $DATE/data_test2.txt
 fi
 
 firefox /persist/home/vawvaw/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/share/doc/rust/html/std/index.html https://adventofcode.com/ 2>&1 >/dev/null&
