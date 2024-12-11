417fa775b5503be75eecc1665e607e31ba9d2410
vaw <valentin@wiedekind1.de>
2024-12-11 11:14:27 +0100

    solve 11/2

--- 11/src/main.zig
+++ 11/src/main.zig
@@ -1,8 +1,15 @@
 const std = @import("std");
 
 const result_type = usize;
+const stone_type = u64;
+const StoneContainer = std.AutoArrayHashMap(stone_type, result_type);
 
-fn task_1(allocator: std.mem.Allocator, file_name: []const u8) !?result_type {
+fn add_stone(container: *StoneContainer, stone: stone_type, n: result_type) !void {
+    const res = try container.getOrPutValue(stone, 0);
+    res.value_ptr.* += n;
+}
+
+fn task_1(allocator: std.mem.Allocator, file_name: []const u8, runs: usize) !?result_type {
     // open input
     var file = try std.fs.cwd().openFile(file_name, .{});
     defer file.close();
@@ -12,47 +19,49 @@ fn task_1(allocator: std.mem.Allocator, file_name: []const u8) !?result_type {
     const data = try input.reader().readAllAlloc(allocator, 1 << 32);
     defer allocator.free(data);
 
-    const stone_type = u64;
-    const List = std.ArrayList(stone_type);
-
-    var old_stones = List.init(allocator);
+    var old_stones = StoneContainer.init(allocator);
     defer old_stones.deinit();
-    var new_stones = List.init(allocator);
+    var new_stones = StoneContainer.init(allocator);
     defer new_stones.deinit();
 
-    var stone_iter = std.mem.tokenizeAny(u8, data, &[_]u8{ ' ', '\n' });
-    while (stone_iter.next()) |token| {
+    var data_iter = std.mem.tokenizeAny(u8, data, &[_]u8{ ' ', '\n' });
+    while (data_iter.next()) |token| {
         const value = try std.fmt.parseInt(stone_type, token, 10);
-        try old_stones.append(value);
+        try add_stone(&old_stones, value, 1);
     }
 
     // loop over data
-    for (0..25) |_| {
+    for (0..runs) |_| {
         new_stones.clearRetainingCapacity();
-        for (old_stones.items) |stone| {
+
+        var stone_iter = old_stones.iterator();
+        while (stone_iter.next()) |entry| {
+            const stone = entry.key_ptr.*;
+            const n = entry.value_ptr.*;
+
             if (stone == 0) {
-                try new_stones.append(1);
+                try add_stone(&new_stones, 1, n);
                 continue;
             }
             const log = std.math.log10(stone);
             if (log % 2 == 1) {
                 const offset = std.math.pow(stone_type, 10, (log + 1) / 2);
-                try new_stones.append(stone / offset);
-                try new_stones.append(stone % offset);
+                try add_stone(&new_stones, stone / offset, n);
+                try add_stone(&new_stones, stone % offset, n);
             } else {
-                try new_stones.append(stone * 2024);
+                try add_stone(&new_stones, stone * 2024, n);
             }
         }
-        std.mem.swap(List, &old_stones, &new_stones);
+        std.mem.swap(StoneContainer, &old_stones, &new_stones);
     }
 
-    return old_stones.items.len;
-}
+    var result: result_type = 0;
+    var iter = old_stones.iterator();
+    while (iter.next()) |entry| {
+        result += entry.value_ptr.*;
+    }
 
-fn task_2(allocator: std.mem.Allocator, file_name: []const u8) !?result_type {
-    _ = allocator;
-    _ = file_name;
-    return error.NotImplemented;
+    return result;
 }
 
 pub fn main() !void {
@@ -70,18 +79,15 @@ pub fn main() !void {
     // execute
     var result: result_type = undefined;
     if (args.len < 2 or args[1][0] != '2') {
-        result = (try task_1(allocator, "data.txt")).?;
+        result = (try task_1(allocator, "data.txt", 25)).?;
     } else {
-        result = (try task_2(allocator, "data.txt")).?;
+        result = (try task_1(allocator, "data.txt", 75)).?;
     }
 
     try stdout.print("{d}\n", .{result});
 }
 
 test task_1 {
-    try std.testing.expectEqual(55312, try task_1(std.testing.allocator, "data_test1.txt"));
-}
-
-test task_2 {
-    // try std.testing.expectEqual(81, try task_2(std.testing.allocator, "data_test1.txt"));
+    try std.testing.expectEqual(22, try task_1(std.testing.allocator, "data_test1.txt", 6));
+    try std.testing.expectEqual(55312, try task_1(std.testing.allocator, "data_test1.txt", 25));
 }
