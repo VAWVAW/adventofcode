9876ce9f2d754e76cf5298912338266fb38c42d7
vaw <valentin@wiedekind1.de>
2024-12-15 19:47:28 +0100

    solve 15/2

--- 15/src/main.zig
+++ 15/src/main.zig
@@ -83,8 +83,8 @@ fn task_1(allocator: std.mem.Allocator, file_name: []const u8) !?result_type {
     // loop over data
     const start_pos_data = std.mem.indexOf(u8, output.items, "@").?;
     var pos = Point{
-        .x = start_pos_data / (line_length.? + 1),
-        .y = start_pos_data % (line_length.? + 1),
+        .y = start_pos_data / (line_length.? + 1),
+        .x = start_pos_data % (line_length.? + 1),
     };
 
     lines.items[pos.y][pos.x] = '.';
@@ -113,10 +113,124 @@ fn task_1(allocator: std.mem.Allocator, file_name: []const u8) !?result_type {
     return result;
 }
 
+fn canMove2(d: Direction, lines: []const []const u8, start: Point) bool {
+    const new = d.move(start);
+    const c = lines[new.y][new.x];
+
+    const other = switch (c) {
+        '#' => return false,
+        '.' => return true,
+
+        '[' => Direction.right.move(new),
+        ']' => Direction.left.move(new),
+        else => unreachable,
+    };
+
+    if (d == .right or d == .left)
+        return canMove2(d, lines, other);
+
+    return canMove2(d, lines, new) and
+        canMove2(d, lines, other);
+}
+fn doMove2(d: Direction, lines: [][]u8, start: Point) void {
+    const new = d.move(start);
+    const c = lines[new.y][new.x];
+
+    const other = switch (c) {
+        '.' => {
+            std.mem.swap(u8, &lines[new.y][new.x], &lines[start.y][start.x]);
+            return;
+        },
+
+        '[' => Direction.right.move(new),
+        ']' => Direction.left.move(new),
+        else => unreachable,
+    };
+
+    doMove2(d, lines, other);
+    if (d == .left or d == .right) {
+        std.mem.swap(u8, &lines[other.y][other.x], &lines[new.y][new.x]);
+    } else {
+        doMove2(d, lines, new);
+    }
+    std.mem.swap(u8, &lines[new.y][new.x], &lines[start.y][start.x]);
+}
+
 fn task_2(allocator: std.mem.Allocator, file_name: []const u8) !?result_type {
-    _ = allocator;
-    _ = file_name;
-    return error.NotImplemented;
+    // open input
+    var file = try std.fs.cwd().openFile(file_name, .{});
+    defer file.close();
+    var input = std.io.bufferedReader(file.reader());
+
+    // read data
+    var buf: [1024]u8 = undefined;
+
+    var output = std.ArrayList(u8).init(allocator);
+    defer output.deinit();
+
+    var line_length: ?usize = null;
+
+    while (try input.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        if (line.len == 0)
+            break;
+
+        try output.ensureUnusedCapacity(line.len * 2 + 1);
+        for (line) |c| {
+            output.appendSliceAssumeCapacity(switch (c) {
+                '#' => "##",
+                'O' => "[]",
+                '.' => "..",
+                '@' => "@.",
+                else => unreachable,
+            });
+        }
+        output.appendAssumeCapacity('\n');
+
+        if (line_length == null)
+            line_length = line.len * 2;
+    }
+
+    var lines = std.ArrayList([:'\n']u8).init(allocator);
+    defer lines.deinit();
+    {
+        var i: usize = 0;
+        while (i < output.items.len) : (i += line_length.? + 1) {
+            try lines.append(output.items[i .. i + line_length.? :'\n']);
+        }
+    }
+
+    // loop over data
+    const start_pos_data = std.mem.indexOf(u8, output.items, "@").?;
+    var pos = Point{
+        .y = start_pos_data / (line_length.? + 1),
+        .x = start_pos_data % (line_length.? + 1),
+    };
+
+    lines.items[pos.y][pos.x] = '.';
+
+    while (true) {
+        const move = input.reader().readByte() catch break;
+        if (move == '\n')
+            continue;
+        const dir = Direction.fromChar(move);
+
+        if (canMove2(dir, lines.items, pos)) {
+            doMove2(dir, lines.items, pos);
+            pos = dir.move(pos);
+        }
+    }
+
+    var result: result_type = 0;
+
+    for (lines.items, 0..) |line, y| {
+        for (line, 0..) |c, x| {
+            if (c == '[') {
+                result += x + 100 * y;
+            }
+        }
+    }
+
+    return result;
 }
 
 pub fn main() !void {
@@ -147,4 +261,6 @@ test task_1 {
     try std.testing.expectEqual(10092, try task_1(std.testing.allocator, "data_test1.txt"));
 }
 
-test task_2 {}
+test task_2 {
+    try std.testing.expectEqual(9021, try task_2(std.testing.allocator, "data_test1.txt"));
+}
