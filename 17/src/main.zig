c87adcd80f138f979d7aa81996102b653bf00c36
vaw <valentin@wiedekind1.de>
2024-12-17 16:56:43 +0100

    solve 17/2

--- 17/src/main.zig
+++ 17/src/main.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 
-const result_type = std.ArrayList(u8);
+const result_type = reg_type;
 
-const reg_type = i32;
+const reg_type = u64;
 const A = 0;
 const B = 1;
 const C = 2;
@@ -122,7 +122,7 @@ test execute {
     output.clearRetainingCapacity();
 }
 
-fn task_1(allocator: std.mem.Allocator, file_name: []const u8) !?result_type {
+fn task_1(allocator: std.mem.Allocator, file_name: []const u8) !?std.ArrayList(u8) {
     // open input
     var file = try std.fs.cwd().openFile(file_name, .{});
     defer file.close();
@@ -149,7 +149,7 @@ fn task_1(allocator: std.mem.Allocator, file_name: []const u8) !?result_type {
     defer allocator.free(program);
 
     // execute program
-    var result = result_type.init(allocator);
+    var result = std.ArrayList(u8).init(allocator);
     errdefer result.deinit();
 
     try execute(&regs, program, result.writer());
@@ -159,10 +159,92 @@ fn task_1(allocator: std.mem.Allocator, file_name: []const u8) !?result_type {
     return result;
 }
 
+fn execute2(regs: *[3]reg_type, program: []const u8, expected: []const u8) !bool {
+    var offset: usize = 0;
+
+    var ip: usize = 0;
+    while (ip < program.len) {
+        const opcode = program[ip];
+        const operand = program[ip + 2];
+
+        switch (executeInstruction(regs, opcode, operand)) {
+            Action.jump => |new_ip| {
+                ip = new_ip * 2;
+                continue;
+            },
+            Action.output => |out| {
+                if (out + '0' != expected[offset]) {
+                    return false;
+                }
+                offset += 2;
+            },
+            Action.none => {},
+        }
+
+        ip += 4;
+    }
+
+    return offset >= expected.len;
+}
+
 fn task_2(allocator: std.mem.Allocator, file_name: []const u8) !?result_type {
-    _ = file_name; // autofix
-    _ = allocator; // autofix
-    return error.NotImplemented;
+    // open input
+    var file = try std.fs.cwd().openFile(file_name, .{});
+    defer file.close();
+    var input = std.io.bufferedReader(file.reader());
+
+    // read data
+    var regs: [3]reg_type = undefined;
+
+    var buf: [32]u8 = undefined;
+    var i: usize = 0;
+    while (try input.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        if (line.len == 0)
+            break;
+
+        const val = try std.fmt.parseInt(reg_type, line["Register A: ".len..line.len], 10);
+        regs[i] = val;
+        i += 1;
+    }
+
+    // skip to program
+    _ = try input.reader().read(buf[0.."Program: ".len]);
+
+    const program = try input.reader().readAllAlloc(allocator, 1 << 16);
+    defer allocator.free(program);
+
+    // execute program
+    var possible = std.AutoArrayHashMap(reg_type, void).init(allocator);
+    defer possible.deinit();
+    var old_possible = std.AutoArrayHashMap(reg_type, void).init(allocator);
+    defer old_possible.deinit();
+
+    try possible.put(0, {});
+
+    for (0..(program.len) / 2) |match_len| {
+        std.mem.swap(std.AutoArrayHashMap(reg_type, void), &possible, &old_possible);
+        possible.clearRetainingCapacity();
+        const expected = program[(program.len - 2 * (match_len + 1))..program.len];
+
+        for (old_possible.keys()) |old_a| {
+            for (0..8) |next_a| {
+                const cur_a = (old_a << 3) + @as(reg_type, @intCast(next_a));
+
+                var current_regs = regs;
+                current_regs[A] = cur_a;
+
+                if (try execute2(&current_regs, program, expected)) {
+                    try possible.put(cur_a, {});
+                }
+            }
+        }
+    }
+
+    var min = possible.keys()[0];
+    for (possible.keys()) |k| {
+        min = @min(min, k);
+    }
+    return min;
 }
 
 pub fn main() !void {
@@ -178,15 +260,17 @@ pub fn main() !void {
     defer std.process.argsFree(allocator, args);
 
     // execute
-    var result: result_type = undefined;
     if (args.len < 2 or args[1][0] != '2') {
+        var result: std.ArrayList(u8) = undefined;
         result = (try task_1(allocator, "data.txt")).?;
-    } else {
-        result = (try task_2(allocator, "data.txt")).?;
-    }
-    defer result.deinit();
+        defer result.deinit();
 
-    try stdout.print("{s}\n", .{result.items});
+        try stdout.print("{s}\n", .{result.items});
+    } else {
+        var result: result_type = undefined;
+        result = (try task_2(allocator, "data.txt")).?;
+        try stdout.print("{d}\n", .{result});
+    }
 }
 
 test task_1 {
@@ -194,6 +278,13 @@ test task_1 {
     defer result.deinit();
 
     try std.testing.expectEqualSlices(u8, "4,6,3,5,6,3,5,2,1,0", result.items);
+
+    const result2 = (try task_1(std.testing.allocator, "data_test2.txt")).?;
+    defer result2.deinit();
+
+    try std.testing.expectEqualSlices(u8, "0,3,5,4,3,0", result2.items);
 }
 
-test task_2 {}
+test task_2 {
+    try std.testing.expectEqual(117440, try task_2(std.testing.allocator, "data_test2.txt"));
+}
